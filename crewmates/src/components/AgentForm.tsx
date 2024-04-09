e6e057c3c2edf9b5c7cb56fa554bb389c7b5f9bd
lausan3 <lauanthony4455@gmail.com>
2024-04-09 13:27:48 -0400

    Moved CreateAgent form to AgentForm

--- crewmates/src/components/AgentForm.tsx
+++ crewmates/src/components/AgentForm.tsx
@@ -1,7 +1,59 @@
-const AgentForm = () => {
+interface AgentFormProps {
+  data: any;
+  selectedOption: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  handleOptionChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  createAgent: (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
+}
+
+const AgentForm = ({data, selectedOption, handleChange, handleOptionChange, createAgent}: AgentFormProps) => {
   return (
     <div className="main-ctn">
-      
+      <form className='create-form'>
+        <label htmlFor="title">Title</label><br />
+        <input type="text" id="title" name="name" value={data.name} onChange={handleChange} /><br />
+        <br/>
+
+        <label htmlFor="description">Description</label><br />
+        <textarea rows={5} cols={50} name="description" value={data.description} onChange={handleChange}>
+        </textarea>
+        <br/>
+        
+        <input
+          type="radio"
+          name="option"
+          value="Duelist"
+          checked={selectedOption === 'Duelist'}
+          onChange={handleOptionChange}
+        />
+        Duelist
+        <input
+          type="radio"
+          name="option"
+          value="Initiator"
+          checked={selectedOption === 'Initiator'}
+          onChange={handleOptionChange}
+        />
+        Initiator
+        <input
+          type="radio"
+          name="option"
+          value="Controller"
+          checked={selectedOption === 'Controller'}
+          onChange={handleOptionChange}
+        />
+        Controller
+        <input
+          type="radio"
+          name="option"
+          value="Sentinel"
+          checked={selectedOption === 'Sentinel'}
+          onChange={handleOptionChange}
+        />
+        Sentinel
+
+        <input type="submit" value="Submit" onClick={createAgent} />
+      </form>
     </div>
   )
 }
--- crewmates/src/routes/CreateAgent.tsx
+++ crewmates/src/routes/CreateAgent.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import { supabase } from '../client'
+import AgentForm from '../components/AgentForm';
 
 const CreateAgent = () => {
-  const [formData, setFormData] = useState({name: null, description: null});
+  const [formData, setFormData] = useState({name: "", description: ""});
   const [selectedOption, setSelectedOption] = useState("");
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedOption(event.target.value);
-    console.log(selectedOption);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -18,22 +18,22 @@ const CreateAgent = () => {
             [name]:value,
         }
     })
-}
+  }
 
   const createAgent = async (event: React.MouseEvent<HTMLInputElement>) => {
     event.preventDefault();
 
     const insertData: { name?: string; description?: string; role?: string } = {};
 
-    if (formData.name) {
+    if (formData.name !== "") {
       insertData.name = formData.name;
     }
 
-    if (formData.description) {
+    if (formData.description !== "") {
       insertData.description = formData.description;
     }
 
-    if (selectedOption != "") {
+    if (selectedOption !== "") {
       insertData.role = selectedOption;
     }
 
@@ -47,51 +47,7 @@ const CreateAgent = () => {
 
   return (
     <div className="main-ctn">
-      <form className='create-form'>
-        <label htmlFor="title">Title</label><br />
-        <input type="text" id="title" name="title" onChange={handleChange} /><br />
-        <br/>
-
-        <label htmlFor="description">Description</label><br />
-        <textarea rows={5} cols={50} id="description" onChange={handleChange}>
-        </textarea>
-        <br/>
-        
-        <input
-          type="radio"
-          name="option"
-          value="Duelist"
-          checked={selectedOption === 'Duelist'}
-          onChange={handleOptionChange}
-        />
-        Duelist
-        <input
-          type="radio"
-          name="option"
-          value="Initiator"
-          checked={selectedOption === 'Initiator'}
-          onChange={handleOptionChange}
-        />
-        Initiator
-        <input
-          type="radio"
-          name="option"
-          value="Controller"
-          checked={selectedOption === 'Controller'}
-          onChange={handleOptionChange}
-        />
-        Controller
-        <input
-          type="radio"
-          name="option"
-          value="Sentinel"
-          checked={selectedOption === 'Sentinel'}
-          onChange={handleOptionChange}
-        />
-        Sentinel
-
-        <input type="submit" value="Submit" onClick={createAgent} />
-      </form>
+      <AgentForm data={formData} selectedOption={selectedOption} handleChange={handleChange} handleOptionChange={handleOptionChange} createAgent={createAgent}/>
     </div>
   )
 }
